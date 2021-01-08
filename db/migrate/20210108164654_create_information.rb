e8a586928d147f5bd5d85f2e2ddf3ea4843024c7
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-09 01:48:01 +0900

    カラムの設定

--- db/migrate/20210108164654_create_information.rb
+++ db/migrate/20210108164654_create_information.rb
@@ -1,7 +1,9 @@
 class CreateInformation < ActiveRecord::Migration[6.0]
   def change
     create_table :information do |t|
-
+      t.string :title, null: false
+      t.text :sentence, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
