57fe672da9dd374b764bfe6807a3d36dbd8100b1
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-09 01:50:11 +0900

    コントローラーの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_152708) do
+ActiveRecord::Schema.define(version: 2021_01_08_164654) do
+
+  create_table "information", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "sentence", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_information_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_01_08_152708) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "information", "users"
 end
