eb5cadc5e7f5ee7782581a08130f395df2c54098
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-10 15:40:22 +0900

    imageカラムの変更

--- db/migrate/20210108164654_create_information.rb
+++ db/migrate/20210108164654_create_information.rb
@@ -3,7 +3,7 @@ class CreateInformation < ActiveRecord::Migration[6.0]
     create_table :information do |t|
       t.string :title, null: false
       t.text :sentence, null: false
-      t.text :image, null: false
+      t.text :image
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
