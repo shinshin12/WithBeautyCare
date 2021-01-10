2da353217e054702c88049fc764c77feed2697de
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-10 23:23:17 +0900

    アップデートのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "informations#index"
-  resources :informations, only: [:new, :create, :show, :edit]
+  resources :informations, only: [:new, :create, :show, :edit, :update]
 end
