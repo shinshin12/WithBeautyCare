2dd97677859a5e2a13fbd6bcabe437d1ab50ab16
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-12 03:21:34 +0900

    記述の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    user = User.find(params[:id])
     @nickname = current_user.nickname
     @informations = current_user.informations
   end
