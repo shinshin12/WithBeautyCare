cbeee43be711392afee172e11e30e68dc7232d81
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-11 16:19:39 +0900

    gemをbootstrapに変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../button")
+require ("jquery3")
+require ("popper")
+require ("bootstrap-sprockets")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
