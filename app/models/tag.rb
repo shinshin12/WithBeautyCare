c43a65c3eb11589c3ecfbfd78b304128d9aa082c
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-13 22:33:08 +0900

    タグ機能を投稿画面に追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,10 +1,10 @@
 class Tag < ActiveHash::Base
  self.data = [
   {id: 1, name: "--"},
-  {id: 2, name:"食事"}
-  {id: 3, name:"スキンケア"}
-  {id: 4, name:"病院"}
-  {id: 5, name:"ステロイド"}
+  {id: 2, name:"食事"},
+  {id: 3, name:"スキンケア"},
+  {id: 4, name:"病院"},
+  {id: 5, name:"ステロイド"},
   {id: 6, name:"その他"}
  ]
 
