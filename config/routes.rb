1478128eaa679dddfb11ad906fded9bb3d0a47ab
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-13 17:07:14 +0900

    パスの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :informations, only: [:new, :create, :show, :edit, :update, :index]
-  resources :users, only: [:show, :edit, :update]
+  resources :users
 end
