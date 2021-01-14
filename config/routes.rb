3a086de8dd02979541980a85d2ef9fae1cf2993a
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-14 17:09:37 +0900

    ルーティングのネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-  resources :informations
+  resources :informations do
+    resources :comments only: :create
+  end
   resources :users
 end
