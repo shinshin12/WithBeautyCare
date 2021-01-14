f2af66f329195b3c5b96187ff9760d445167fa88
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-14 17:17:18 +0900

    createアクションの記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+
+  def create
+     @comments = Comment.create(comment_params)
+     redirect_to information_path(comment.information.id)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:information_id])
+  end
 end
