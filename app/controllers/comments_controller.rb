b63f03ef32b4b4fb12c7cc400d1f8aad42be7f6c
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-14 17:39:50 +0900

    コメントの表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,11 @@ class CommentsController < ApplicationController
 
   def create
      @comments = Comment.create(comment_params)
-     redirect_to information_path(comment.information.id)
+     redirect_to information_path(@comments.information.id)
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:information_id])
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, information_id: params[:information_id])
   end
 end
