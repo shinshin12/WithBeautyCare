7824839103e78216ec8b609c33dc2466e496a301
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-14 17:11:20 +0900

    コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :informations do
-    resources :comments only: :create
+    resources :comments, only: :create
   end
   resources :users
 end
