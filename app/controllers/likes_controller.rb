2e20b98f03b1cbe6620ba1aa20fa1adb3c69ea82
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-18 17:57:41 +0900

    コントローラーの記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,19 @@
 class LikesController < ApplicationController
+  before_action :move_like
+  def create
+    user = current_user
+    information = Information.find(params[:information_id])
+    like = Like.create(user_id: user.id, information_id: information.id)
+  end
+  def destroy
+    user = current_user
+    information = Information.find(params[:information_id])
+    like = Like.find_by(user_id: user.id, information_id: information.id)
+    like.delete
+  end
+
+    private
+  def move_like
+    @information = Information.find(params[:information_id])
+  end
 end
