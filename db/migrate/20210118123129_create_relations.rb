1dcd31721e6177764eee1beb29aa6f7acbba0758
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-18 21:40:30 +0900

    アソシエーション の記述

--- db/migrate/20210118123129_create_relations.rb
+++ db/migrate/20210118123129_create_relations.rb
@@ -4,6 +4,10 @@ class CreateRelations < ActiveRecord::Migration[6.0]
       t.references :user, foreign_key: true
       t.references :follow, foreign_key: { to_table: :users }
       t.timestamps
+
+
+
+      t.index [:user_id, :follow_id], unique: true
     end
   end
 end
