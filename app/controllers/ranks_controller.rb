b790345fe511b6d0f248c7abad5ac7175fd77d78
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-19 18:47:04 +0900

    いいねランキングのview

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,6 +1,6 @@
 class RanksController < ApplicationController
   def index
     @rank_information = Information.order(impression_count: "DESC").limit(15)
-    
+    @like_information = Information.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}.limit(15)
   end
 end
--- app/models/rank.rb
+++ app/models/rank.rb
@@ -2,5 +2,6 @@ class Rank < ApplicationRecord
   belongs_to :information
   belongs_to :user
   has_many :likes
+  has_many :liked_users, through: :likes, source: :user
   is_impressionable counter_chache: true
 end
