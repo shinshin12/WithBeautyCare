b4d1cde50030d909513a8aeb172ce72795489671
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-20 17:06:24 +0900

    いいね数の表示

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -5,6 +5,7 @@ class RanksController < ApplicationController
     @rank_information = Information.order(impression_count: "DESC")
   end
   def put
+    @like_information = Information.all
     @like_information = Information.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
   end
 end
