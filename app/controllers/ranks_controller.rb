da878906bebc96103669c86a21b275a059b21007
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-21 22:15:30 +0900

    投稿数制限

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,16 +1,15 @@
 class RanksController < ApplicationController
-  PER = 10
 
   def set
     @rank_information = Information.all
-    @rank_information = Information.order(impression_count: "DESC").page(params[:page]).per(PER)
+    @rank_information = Information.order(impression_count: "DESC").limit(10)
   end
   def put
     @like_information = Information.all
-    @like_information = Information.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}.page(params[:page]).per(PER)
+    @like_information = Information.includes(:liked_users).limit(10).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
   end
   def read
     @user_relations = User.all
-    @user_relations = Relation.group(:followed_id).order('count(followed_id) DESC').page(params[:page]).per(PER)
+    @user_relations = Relation.group(:followed_id).order('count(followed_id) DESC').limit(10)
   end
 end
