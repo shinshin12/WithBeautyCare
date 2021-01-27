538b602266835e774d10ac9b280a601ad73425fc
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-27 23:54:06 +0900

    serchform　削除　コード

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,1 +1,17 @@
-console.log("読み込み")
+
+
+document.addEventListener("DOMContentLoaded", function(){
+let searching = document.getElementById("search")
+let button = document.getElementById("btn")
+let button2 = document.getElementById("btn2")
+
+searching.addEventListener("mouseout", function(){
+  let child = button.parentElement;
+  child.removeChild(button)
+
+  let child2 = button2.parentElement;
+  child2.removeChild(button2)
+})
+
+
+})
