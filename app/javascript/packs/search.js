03ac7d738294a4adf6ec2d6dcf912cadd344d1fc
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-28 03:36:57 +0900

    DOM操作

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,17 +1,14 @@
+const { removeData } = require("jquery")
 
 
 document.addEventListener("DOMContentLoaded", function(){
-let searching = document.getElementById("search")
-let button = document.getElementById("btn")
-let button2 = document.getElementById("btn2")
 
-searching.addEventListener("mouseout", function(){
-  let child = button.parentElement;
-  child.removeChild(button)
+let searching = document.getElementById("search");
+let button = document.getElementById("btn");
+let button2 = document.getElementById("btn2");
 
-  let child2 = button2.parentElement;
-  child2.removeChild(button2)
-})
-
-
-})
+searching.addEventListener("mouseover", function(){
+  button.setAttribute("style", "displey:block;");
+  button2.setAttribute("style", "displey:block;");
+ });
+});
