e0b4132a1871eee10de5e3d957e0822b370a8ea4
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-29 00:21:17 +0900

    jqueryの修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require ("jquery") 
 require("@rails/ujs").start()
+require("turbolinks").start();
 require("@rails/activestorage").start()
 require("channels")
+require ("jquery") 
 require ("./file")
 require ("./search")
 require ("./count")
