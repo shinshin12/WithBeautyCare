c8fd784882a2af15f4f3374b16946e06db3f2751
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-01-30 22:34:05 +0900

    通信に失敗した場合の記述

--- app/javascript/packs/postal.js
+++ app/javascript/packs/postal.js
@@ -4,7 +4,13 @@ $(function (){
        url: "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + $("#zipcode").val(),
          dataType : "jsonp",
     }).done(function(data) {
-        console.log(data)
-    });
+       if(data.results){
+
+       }else{
+         alert("該当するデータが見つかりませんでした")
+       }
+    }).fail(function(data){
+      alert("通信に失敗しました")
+    })
  });
 });
