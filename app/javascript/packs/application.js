fa08a2e2d1d7cc16de0d77cdfb6d1babae324e32
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-02-01 23:14:33 +0900

    configの追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,6 @@ require ("./search")
 require ("./count")
 require ("./postal")
 require ("./map")
-require ("./mapping")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
