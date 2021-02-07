e42394da8d70483a81b76b2fc3ec19d6496aea64
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-02-07 17:42:31 +0900

    修正

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,6 +1,10 @@
-let http = require('http'); //httpモジュール呼び出し
-http.createServer(function (request, response) {
-    // リクエストを受けると以下のレスポンスを送信する
-    response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
-    response.end('Hello World\n'); // レスポンスボディに書き込み＆レスポンス送信を完了する
-}).listen(process.env.PORT || 8080); //公開ポートで待ち受け
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 8080
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
