88c2da099910665874dc8528fa6d1c5eac13b84a
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-02-10 14:48:19 +0900

    デプロイのため修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,8 @@ class ImageUploader < CarrierWave::Uploader::Base
    include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-    storage :fog
+  storage :file
+  # storage :fog
   
 
   # Override the directory where uploaded files will be stored.
@@ -38,9 +39,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-   def extension_whitelist
-    %w(jpg jpeg gif png)
-   end
+   #def extension_whitelist
+   #%w(jpg jpeg gif png)
+   #end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
