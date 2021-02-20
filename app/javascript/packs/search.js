5231e3025e5dd8fc9584c7efbea6b5a6ee2afd48
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-02-20 19:17:20 +0900

    jqueryに記述変更

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,12 +1,7 @@
-
-document.addEventListener("DOMContentLoaded", function(){
-
-let searching = document.getElementById("search");
-let button = document.getElementById("btn");
-let button2 = document.getElementById("btn2");
-
-searching.addEventListener("click", function(){
-  button.setAttribute("style", "displey:block;");
-  button2.setAttribute("style", "displey:block;");
- });
+$(function(){
+  $(document).on("click","#search",function(){
+    $("#button").attr("style", "display:block;")
+    $("#button2").attr("style", "display:block;")
+  });
 });
+
