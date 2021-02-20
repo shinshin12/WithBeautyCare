a9976c1c8351bab97ce5ead42dd65198324b1317
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2021-02-20 18:24:05 +0900

    閲覧カウンターの編集

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,7 +1,7 @@
 class RanksController < ApplicationController
   def set
     @rank_information = Information.all
-    @rank_information = Information.order(impression_count: "DESC").limit(10)
+    @rank_information = Information.order(impressions_count: "DESC").limit(10)
   end
   def put
     @like_information = Information.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
--- db/migrate/20210119064515_add_impressions_count_to_informations.rb
+++ db/migrate/20210119064515_add_impressions_count_to_informations.rb
@@ -1,5 +1,5 @@
 class AddImpressionsCountToInformations < ActiveRecord::Migration[6.0]
   def change
-    add_column :information, :impression_count, :integer, default:0
+    add_column :information, :impressions_count, :integer, default:0
   end
 end
